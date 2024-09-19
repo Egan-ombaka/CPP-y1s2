b799f4e3ccea368e0d67347e96883374d9d590a2
Egan-ombaka <eganombaka97@gmail.com>
2024-09-19 10:58:40 +0300

    +,0 or - number

---
+++ negativeorpositive.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int number;
+
+    cout << "Enter the Number" << endl;
+    cin >> number;
+
+    if( number > 0)
+       cout << number << "is a Positive Number" << endl;
+    else if (number < 0)
+       cout << number << "is a negative Number"  << endl;
+    else
+       cout << number << "is Zero" << endl;
+
+    return 0;
+
+}
