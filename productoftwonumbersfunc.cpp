edb1dc6771a4ea286c9f7eac223f9b18d0e480f4
Egan-ombaka <eganombaka97@gmail.com>
2024-09-26 15:19:42 +0300

    simple interest function

--- productoftwonumbersfunc.cpp
+++ productoftwonumbersfunc.cpp
@@ -2,32 +2,20 @@
 
 #include<iostream>
 using namespace std;
-int product(int a, int b);
 int simpleinterest(int p, int r, int t);
 
 int main(){
     int ans, a, b  ,ans_si, p, r, t;
-    //cout<<"Enter the two number"<< endl;
-    //cin>>a>>b;
+
 
     cout<<"Enter the principle, rate, time";
     cin>>p>>r>>t;
 
-   // ans = product(a,b);
     ans_si = simpleinterest(p,r,t);
-
     cout<<" The simple intrest is "<<ans_si <<endl;
 
-    //cout<<" The answer is "<< ans<< endl;
 
     return 0;
-}
-
-int product(int a, int b){
-    int c;
-
-    c = a * b;
-    return c;
 }
 
 
