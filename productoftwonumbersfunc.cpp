941aeb2ac2062b0077e0c6b038b2fdfc71ab23f1
Egan-ombaka <eganombaka97@gmail.com>
2024-09-27 16:12:50 +0300

    A C++ function that returns the simple interest

--- productoftwonumbersfunc.cpp
+++ productoftwonumbersfunc.cpp
@@ -1,12 +1,15 @@
-// a function to find the product o two numbers
+/*
+By: Egan Ombaka
+Date: 26/09/2024
+A C++ function that returns the simple interest
+*/
 
 #include<iostream>
 using namespace std;
 int simpleinterest(int p, int r, int t);
 
 int main(){
-    int ans, a, b  ,ans_si, p, r, t;
-
+    int ans_si, p, r, t;
 
     cout<<"Enter the principle, rate, time";
     cin>>p>>r>>t;
