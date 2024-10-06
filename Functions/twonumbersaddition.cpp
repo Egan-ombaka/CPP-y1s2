114d72c89f6763702159bf0380107b763ad202b0
Egan-ombaka <eganombaka97@gmail.com>
2024-10-06 16:52:27 +0300

    two numbers sum

--- Functions/twonumbersaddition.cpp
+++ Functions/twonumbersaddition.cpp
@@ -1,1 +1,36 @@
-__has_include
+/*
+Author: Egan Ombaka
+Date: 6/10/2024
+This is a c++ function that returns a sum of two numbers
+*/
+
+#include<iostream>
+using namespace std;
+double sum_calculation(double a, double b);
+
+//function to calculate the sum of the two numbers
+double sum_calculation(double a, double b){
+    double sum; //declaring the two numbers a the variable to be returned
+
+    //calcuating the sum
+    sum = a + b;
+
+    return sum;//returning the answer
+}
+
+int main(){
+    double a,b,ans_sum;//declaring the variables
+
+    //ask the user to the variables
+    cout<<"Enter a"<< endl;
+    cin>>a;
+
+    cout<<"Enter b"<< endl;
+    cin>>b;
+
+    ans_sum = sum_calculation( a,b);// calling the sum_calculation function
+
+    //printing out the answer...
+    cout<<"the sum of a and b is : "<< endl;
+
+}
