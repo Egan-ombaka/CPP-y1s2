1090841d2dd4d74d26af064b1945b512e2714719
egan-ombaka <eganombaka97@gmail.com>
2024-11-02 11:28:17 +0300

    cpp

---
+++ classes/color of a shape.cpp
@@ -0,0 +1,30 @@
+/*
+By: Egan Ombaka
+*/
+
+#include<iostream>
+using namespace std;
+
+//Base class
+class Shape{
+    protected:v
+
+    public:
+    void setColor(string c) {
+        color = c;
+    }
+
+    string getColor() {
+        return color;
+    }
+};
+
+int main(){
+    Shape shape; //creating on object shape
+
+    shape.setColor("Red");
+
+    cout << "The color of the shape is: "<< shape.getColor() <<endl;
+
+    return 0;
+}
